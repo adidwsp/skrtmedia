e9bdb410f94ca1aa9794be6fa05c0020b9395944
adi3947 <adi3947@gmail.com>
2025-11-11 23:51:18 +0700

    remove bug2

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,8 +1,9 @@
 import { redirect } from 'next/navigation'
-import { createServerSupabase } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
 export default async function PrivatePage() {
-  const supabase = await createServerSupabase()
+  const supabase = await createClient(cookies())
   const { data, error } = await supabase.auth.getUser()
   if (error || !data?.user) redirect('/login')
   return <p>Hello {data.user.email}</p>
--- src/app/signin/actions.ts
+++ src/app/signin/actions.ts
@@ -1,12 +1,13 @@
 // server action
-import { createServerSupabase } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
 export async function login(formData: FormData) {
   'use server'
   const email = formData.get('email') as string
   const password = formData.get('password') as string
 
-  const supabase = await createServerSupabase()
+  const supabase = await createClient(cookies())
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password
